4f3a281b23d9b5d1345c3dd2dc37268095d20f9b
AnaGolic <96030312+AnaGolic@users.noreply.github.com>
2022-05-19 19:36:52 +0200

    added some changes in main.js

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,13 @@
-const toggleAnswer = (e) => {
-    const question = e.childNodes[1];
-    hideAllAnswers(question);
-    e.childNodes[3].classList.toggle("rotate-icon");
-    e.classList.toggle("bold");
-    e.nextElementSibling.classList.toggle("hide");
-  };
-  
-  //hide answers
-  
-  const hideAllAnswers = (question) => {
-    const questionsEl = [...document.querySelectorAll(".question")];
-    questionsEl.forEach((questionEl) => {
-      console.log();
-      if (questionEl.childNodes[1] != question) {
-        questionEl.classList.remove("bold");
-        questionEl.nextElementSibling.classList.add("hide");
-        questionEl.childNodes[3].classList.remove("rotate-icon");
-      }
-    });
-  };
+let clickable = document.querySelectorAll(".expand");
+let len = clickable.length;
+
+for(let i=0; i < len; i++){
+
+  clickable[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("hidden");
+
+  })
+
+}
